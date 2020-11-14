003b7bcd902461f9828909b30700ec1931910ebb
kenia <keniavalladarez@gmail.com>
2020-11-13 20:07:29 -0500

    Connect graphQL to our Sanity dataset

--- starter-files/gatsby/gatsby-config.js
+++ starter-files/gatsby/gatsby-config.js
@@ -1,7 +1,23 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
 export default {
   siteMetadata: {
     title: `Slicks Slices`,
     siteUrl: 'https://gatsby.pizza',
     description: 'The best pizza in NYC',
   },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: '4ao18zac',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
 };
