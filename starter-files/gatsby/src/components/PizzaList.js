7000de4570b8903860e50d0f616d1cab31e0ed4b
kenia <keniavalladarez@gmail.com>
2020-11-16 19:52:52 -0500

    Fix typo

--- starter-files/gatsby/src/components/PizzaList.js
+++ starter-files/gatsby/src/components/PizzaList.js
@@ -31,9 +31,9 @@ function SinglePizza({ pizza }) {
         <h2>
           <span className="mark">{pizza.name}</span>
         </h2>
-        <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
-        <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
       </Link>
+      <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
     </PizzaStyles>
   );
 }
