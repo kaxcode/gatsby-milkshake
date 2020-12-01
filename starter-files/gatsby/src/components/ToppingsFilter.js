bf8472c1841762a6959989b776e5cf7610d26701
kenia <keniavalladarez@gmail.com>
2020-12-01 17:25:26 -0500

    Create ToppingsFilter component with styles

---
+++ starter-files/gatsby/src/components/ToppingsFilter.js
@@ -0,0 +1,82 @@
+import { graphql, useStaticQuery, Link } from 'gatsby';
+import React from 'react';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    text-decoration: none;
+    font-size: clamp(1.5rem, 1.4vw, 2.5rem);
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    &[aria-current='page'] {
+      background: var(--yellow);
+    }
+  }
+`;
+
+function countPizzasInTopping(pizzas) {
+  // return pizzas with counts
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        existingTopping.count += 1;
+      } else {
+        acc[topping.id] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
+
+export default function ToppingsFilter() {
+  const { pizzas } = useStaticQuery(graphql`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            name
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  const toppingsWithCounts = countPizzasInTopping(pizzas.nodes);
+
+  console.log(toppingsWithCounts);
+  return (
+    <ToppingsStyles>
+      {toppingsWithCounts.map((topping) => (
+        <Link to={`/topping/${topping.name}`} key={topping.id}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingsStyles>
+  );
+}
