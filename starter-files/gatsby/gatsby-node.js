9d40771455d48223952e0f7815a0c21a1b6427af
kenia <keniavalladarez@gmail.com>
2020-12-02 16:48:29 -0500

    Create dynamic pages for each pizza

---
+++ starter-files/gatsby/gatsby-node.js
@@ -0,0 +1,34 @@
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+    });
+  });
+}
+
+export async function createPages(params) {
+  // Create pages dynamically
+  // 1. Pizzas
+  await turnPizzasIntoPages(params);
+  // 2. Toppings
+
+  // 3. Slicemasters
+}
