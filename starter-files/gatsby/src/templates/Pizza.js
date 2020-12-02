b4632988fb931573ff3cb9cd19ed23240644d0eb
kenia <keniavalladarez@gmail.com>
2020-12-02 17:06:06 -0500

    Style the single pizza pages

--- starter-files/gatsby/src/templates/Pizza.js
+++ starter-files/gatsby/src/templates/Pizza.js
@@ -1,8 +1,28 @@
 import { graphql } from 'gatsby';
 import React from 'react';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage() {
-  return <p>Single Pizza</p>;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+`;
+
+export default function SinglePizzaPage({ data: { pizza } }) {
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 export const query = graphql`
@@ -10,6 +30,18 @@ export const query = graphql`
     pizza: sanityPizza(slug: { current: { eq: $slug } }) {
       name
       id
+      image {
+        asset {
+          fluid(maxWidth: 800) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+      toppings {
+        name
+        id
+        vegetarian
+      }
     }
   }
 `;
